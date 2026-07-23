4117868125706afebacc8ada89e68b8cd93576e0
Wellen <wellenatwiine256@gmail.com>
2026-07-23 20:50:15 +0000

    Switch port to 10000 for Render deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 celery -A tasks worker --loglevel=info --concurrency=1 &
-uvicorn main:app --host 0.0.0.0 --port 7860 --workers 1
+uvicorn main:app --host 0.0.0.0 --port 10000 --workers 1
